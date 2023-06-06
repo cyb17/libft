02f5d3a65181b3e7c0b33acdff16c16676f04e3e
cyb17 <124796426+cyb17@users.noreply.github.com>
2023-06-06 13:11:45 +0200

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,57 +12,27 @@
 
 #include "libft.h"
 
-static int	ft_check_sign(const char *str)
+long	ft_atoi(const char *str)
 {
-	int	i;
-	int	c;
-	int	sign;
+	long	i;
+	long	number;
+	int		sign;
 
 	i = 0;
-	c = 0;
-	sign = 43;
-	while (str[i] == 32 || (str[i] < 14 && str[i] > 8))
+	number = 0;
+	sign = 1;
+	while (str[i] && (str[i] == 32 || (str[i] >= 9 && str[i] <= 13)))
 		i++;
-	while (str[i] == 45 || str[i] == 43)
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (str[i] == 45)
-			sign = 45;
+		if (str[i] == '-')
+			sign = -1;
 		i++;
-		c++;
 	}
-	if (c > 1)
-		return (0);
-	else
-		return (sign);
-}
-
-int	ft_atoi(const char *str)
-{
-	int	i;
-	int	nbr;
-
-	i = 0;
-	nbr = 0;
-	if (ft_check_sign(str) == 45 || ft_check_sign(str) == 43)
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		while (str[i] == 32 || (str[i] < 14 && str[i] > 8))
-			i++;
-		while (str[i] == 45 || str[i] == 43)
-			i++;
-		while (str[i] <= '9' && str[i] >= '0')
-		{
-			nbr = nbr * 10 + (str[i] - 48);
-			i++;
-		}
+		number = (number * 10) + (str[i] - '0');
+		i++;
 	}
-	if (ft_check_sign(str) == 45)
-		return (nbr * (-1));
-	return (nbr);
+	return (number * sign);
 }
-/*
-#include <stdio.h>
-
-int	main()
-{
-	printf("%d", ft_atoi("  		+42"));
-}*/
